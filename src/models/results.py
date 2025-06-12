76e72a3be2cbac28a25b88a9a19cd103b8f90bbd
student-zp-41 <dementiev.kos04@gmail.com>
2025-06-12 20:45:21 +0700

    add tables 6

--- src/models/results.py
+++ src/models/results.py
@@ -18,12 +18,29 @@ class PlaceORM(Base):
     )
     name: Mapped[str] = mapped_column(String(10))
 
-    results: Mapped[List["ResultORM"]] = relationship(
-        "ResultORM",
+    results: Mapped[List["KarateKumiteResultORM"]] = relationship(
+        "KarateKumiteResultORM",
         back_populates="place",
         cascade='all, delete'
     )
 
+
+class SportTypeORM(Base):
+    __tablename__ = 'sport_types'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),  # нативный тип UUID PostgreSQL
+        primary_key=True,
+        default=uuid.uuid4,  # передаём функцию, не вызываем
+    )
+    name: Mapped[str] = mapped_column(String(100), unique=True)
+    code: Mapped[str] = mapped_column(String(100), unique=True)
+
+    results: Mapped[List["ResultORM"]] = relationship(
+        "ResultORM",
+        back_populates="sport_type",
+        passive_deletes=True
+    )
 
 
 class ResultORM(Base):
@@ -40,24 +57,14 @@ class ResultORM(Base):
     event_id: Mapped[Optional[uuid.UUID]] = mapped_column(
         ForeignKey('events.id', ondelete='SET NULL')
     )
-    place_id: Mapped[Optional[uuid.UUID]] = mapped_column(
-        ForeignKey('places.id', ondelete='SET NULL')
+    sport_type_id: Mapped[Optional[uuid.UUID]] = mapped_column(
+        ForeignKey('sport_types.id', ondelete='RESTRICT'),
     )
-    points_scored: Mapped[int]
-    points_missed: Mapped[int]
-    number_of_fights: Mapped[int]
-    average_score: Mapped[Decimal] = mapped_column(
-        Numeric(5, 2), nullable=False,
-    )
-    efficiency: Mapped[Decimal] = mapped_column(
-        Numeric(5, 2), nullable=False,
-    )
+    sport_code: Mapped[Optional[str]] = mapped_column(String(50))
+
+
     event: Mapped["EventORM"] = relationship(
         "EventORM",
-        back_populates="results"
-    )
-    place: Mapped["PlaceORM"] = relationship(
-        "PlaceORM",
         back_populates="results"
     )
 
@@ -65,3 +72,39 @@ class ResultORM(Base):
         "StudentProfileORM",
         back_populates="results"
     )
+    sport_type: Mapped["SportTypeORM"] = relationship(
+        "SportTypeORM",
+        back_populates="results"
+    )
+
+    __mapper_args__ = {
+        "polymorphic_on": sport_code,
+        "polymorphic_identity": "base"
+    }
+
+
+
+class KarateKumiteResultORM(ResultORM):
+    __tablename__ = 'karate_kumite_results'
+
+    id: Mapped[uuid.UUID] = mapped_column(ForeignKey('results.id', ondelete='CASCADE'), primary_key=True)
+
+    place_id: Mapped[Optional[uuid.UUID]] = mapped_column(
+        ForeignKey('places.id', ondelete='SET NULL')
+    )
+    number_of_fights: Mapped[int]
+    number_of_wins: Mapped[int]
+    number_of_defeats: Mapped[int]
+    points_scored: Mapped[int]
+    points_missed: Mapped[int]
+    average_score: Mapped[Decimal] = mapped_column(Numeric(5, 2))
+    efficiency: Mapped[Decimal] = mapped_column(Numeric(5, 2))
+
+    place: Mapped["PlaceORM"] = relationship(
+            "PlaceORM",
+            back_populates="results"
+        )
+
+    __mapper_args__ = {
+        "polymorphic_identity": "karate"
+    }
